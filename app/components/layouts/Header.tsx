949310a85bf27e37bcca740e567dead75b06c54c
Himanshu Basra <himanshubasra10@gmail.com>
2025-04-19 21:53:47 -0600

    initial commit

---
+++ app/components/layouts/Header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <ul>
+                    <li className="text-3xl">
+                        Habit tracker
+                    </li>
+                </ul>
+            </div>    
+        </header>
+
+        
+    );
+}
+export default Header;
