3c3ba75ab9459fed20ad9a34d0ba07983eb41ef3
Himanshu Basra <himanshubasra10@gmail.com>
2025-04-19 21:53:47 -0600

    initial commit

---
+++ app/components/layouts/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="footer footer-center p-10 bg-neutral text-neutral-content">
+      <div>
+        <p>Copyright © 2023 - All right reserved by Habit Tracker</p>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
